528307191cdf19c5a4194c418077ea6ad7cde886
David Stringer <stringer.david@gmail.com>
2011-10-21 21:39:24 -0700

    the files for the custom gem

---
+++ lib/jdbc/sqlite3.rb
@@ -0,0 +1,10 @@
+module Jdbc
+  module SQLite3
+    VERSION = "3.7.7.1.PINGID.2"
+  end
+end
+if RUBY_PLATFORM =~ /java/
+  require "sqlite-jdbc-#{Jdbc::SQLite3::VERSION}.jar"
+elsif $VERBOSE
+  warn "jdbc-SQLite3 is only for use with JRuby"
+end
